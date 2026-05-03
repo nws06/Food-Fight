afa9a4e77a6b44740857ed855561232fd8d8467f
Noah Stacey <noahstacey1006@gmail.com>
2026-05-02 20:43:02 -0400

    Set up player base stats
    Movement, damage, fire rate, etc.
    Some things that will be altered by upgrades

---
+++ Assets/Scripts/ScriptableObjects/PlayerBaseStats.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerBaseStats", menuName = "Scriptable Objects/PlayerBaseStats")]
+public class PlayerBaseStats : ScriptableObject
+{
+    [SerializeField] private float _baseMoveSpeed;
+    [SerializeField] private float _baseHealth;
+
+    [SerializeField] private float _baseDamage;
+    [SerializeField] private float _baseReloadTime;
+    [SerializeField] private float _baseShotCooldown;
+    [SerializeField] private float _baseBulletSpeed;
+    [SerializeField] private int _baseMaxAmmo;
+
+
+
+    public float BaseMoveSpeed { get => _baseMoveSpeed; }
+    public float BaseHealth { get => _baseHealth; }
+
+    public float BaseDamage { get => _baseDamage; }
+    public float BaseReloadTime { get => _baseReloadTime; }
+    public float BaseShotCooldown { get => _baseShotCooldown; }
+    public float BaseBulletSpeed { get => _baseBulletSpeed; }
+    public int BaseMaxAmmo { get => _baseMaxAmmo; }
+}
