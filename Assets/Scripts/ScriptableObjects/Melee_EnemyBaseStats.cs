94918e509e28081594b71276ec24e5eb0effb933
Noah Stacey <noahstacey1006@gmail.com>
2026-05-04 18:43:30 -0400

    Update enemy base stats to include min and max sizes

--- Assets/Scripts/ScriptableObjects/Melee_EnemyBaseStats.cs
+++ Assets/Scripts/ScriptableObjects/Melee_EnemyBaseStats.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Melee_EnemyBaseStats", menuName = "Scriptable Objects/Melee_EnemyBaseStats")]
@@ -8,10 +9,16 @@ public class Melee_EnemyBaseStats : ScriptableObject
 
     [SerializeField] private float _baseDamage;
 
+    [SerializeField] private float _minSize;
+    [SerializeField] private float _maxSize;
+
 
 
     public float BaseMoveSpeed { get => _baseMoveSpeed; }
     public float BaseHealth { get => _baseHealth; }
 
     public float BaseDamage { get => _baseDamage; }
+
+    public float MinSize { get => _minSize; }
+    public float MaxSize { get => _maxSize; }
 }
