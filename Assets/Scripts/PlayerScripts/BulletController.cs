decd9285b63157e0d7c3eaca6bf4efceca21930f
Noah Stacey <noahstacey1006@gmail.com>
2026-05-06 21:32:09 -0400

    Rework hit detection and lifetime

--- Assets/Scripts/PlayerScripts/BulletController.cs
+++ Assets/Scripts/PlayerScripts/BulletController.cs
@@ -1,39 +1,18 @@
-using System.Collections;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class BulletController : MonoBehaviour, IPauseableFixedUpdate
+public class BulletController : MonoBehaviour
 {
-    [SerializeField] private PlayerBaseStats _baseStats;
-    private float _bulletSpeed;
-    private Rigidbody2D _rigidbody;
+    public static event Action<BulletController, Collider2D> OnBulletCollidesEnemy;
+    public Rigidbody2D _rigidbody;
+    public float _spawnTime;
 
 
 
     void Awake()
     {
-        _bulletSpeed = _baseStats.BaseBulletSpeed;
-
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    void OnEnable()
-    {
-        UpdateManager.Instance.RegisterForFixedUpdate(this);
-
-        PauseManager.OnGamePause += OnGamePause;
-    }
-
-
-
-    public void OnPauseableFixedUpdate(float deltaTime)
-    {
-        _rigidbody.linearVelocity = transform.up * _bulletSpeed;
-    }
-
-    void OnGamePause()
-    {
-        _rigidbody.linearVelocity = Vector2.zero;
+        _spawnTime = Time.time;
     }
 
 
@@ -42,16 +21,7 @@ public class BulletController : MonoBehaviour, IPauseableFixedUpdate
     {
         if (otherCollider.CompareTag("Enemy"))
         {
-            Melee_EnemyManager.Instance.DamageEnemy(otherCollider.GetComponent<Melee_EnemyController>(), _baseStats.BaseDamage);
+            OnBulletCollidesEnemy?.Invoke(this, otherCollider);
         }
-    }
-
-
-
-    void OnDisable()
-    {
-        UpdateManager.Instance.UnregisterFromFixedUpdate(this);
-
-        PauseManager.OnGamePause -= OnGamePause;
     }
 }
