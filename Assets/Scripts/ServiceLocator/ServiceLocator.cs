030d65ed7b061be21716020bfb69a8924d901d64
Noah Stacey <noahstacey1006@gmail.com>
2026-05-08 16:56:34 -0400

    Set up ServiceLocator
    Allows other objects to create references to external services.
    Replaces most singletons

---
+++ Assets/Scripts/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,34 @@
+// From https://www.youtube.com/watch?v=maDpJL3go9g
+// "STOP Using Singletons! Try THIS Instead – Service Locator in Unity" 
+// by Binary Impact on YouTube
+
+using System;
+using System.Collections.Generic;
+
+public static class ServiceLocator
+{
+    private static Dictionary<Type, object> _services = new();
+
+
+
+    public static void Register<T>(T service) where T : class {
+        _services[typeof(T)] = service;
+    }
+
+
+
+    public static T Get<T>() where T : class
+    {
+        if (_services.TryGetValue(typeof(T), out object service))
+            return service as T;
+        
+        throw new Exception($"ServiceLocator.cs: Service {typeof(T).Name} was not found.");
+    }
+
+
+
+    public static void Unregister<T>() where T : class
+    {
+        _services.Remove(typeof(T));
+    }
+}
